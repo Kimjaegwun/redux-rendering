d9cb0d3869fe9ccd23c63a5632f655622265739c
kimjaegwun <rlawornjs4@naver.com>
2022-09-28 17:35:25 +0900

    rev

--- src/containers/PhotoListContainer.js
+++ src/containers/PhotoListContainer.js
@@ -1,8 +1,20 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { setBackground } from '../redux/background';
 import { fetchPhotos } from '../redux/photos';
 import PhotoList from '../components/PhotoList';
+import { createSelector } from '@reduxjs/toolkit';
+
+const createPhotos = createSelector(
+  [state => state.photos.data, state => state.category.category],
+  (getphotos, category) => {
+    return category === 'all'
+      ? getphotos
+      : getphotos.filter(photo => {
+          return photo.category === category;
+        });
+  }
+);
 
 function PhotoListContainer() {
   const dispatch = useDispatch();
@@ -11,14 +23,15 @@ function PhotoListContainer() {
     dispatch(fetchPhotos());
   }, [dispatch]);
 
-  const { photos, loading, backgroundColor } = useSelector(state => ({
-    photos:
-      state.category.category === 'all'
-        ? state.photos.data
-        : state.photos.data.filter(photo => photo.category === state.category.category),
-    loading: state.photos.loading,
-    backgroundColor: state.background.background,
-  }));
+  const photos = useSelector(createPhotos);
+
+  const { loading, backgroundColor } = useSelector(
+    state => ({
+      loading: state.photos.loading,
+      backgroundColor: state.background.background,
+    }),
+    shallowEqual
+  );
 
   if (loading === 'error') {
     return <span>Error!</span>;
